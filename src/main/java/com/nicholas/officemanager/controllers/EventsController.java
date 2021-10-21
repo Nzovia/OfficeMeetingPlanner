52f713a79c80497d1cf202e6731cbd080d217643
Nicholas Maundu <nickiemaundu19@gmail.com>
2021-10-21 17:58:54 +0300

    CRUD operation on events

--- src/main/java/com/nicholas/officemanager/controllers/EventsController.java
+++ src/main/java/com/nicholas/officemanager/controllers/EventsController.java
@@ -1,5 +1,6 @@
 package com.nicholas.officemanager.controllers;
 
+import com.nicholas.officemanager.entitities.Employees;
 import com.nicholas.officemanager.entitities.Events;
 import com.nicholas.officemanager.repositories.EventsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,7 +18,7 @@ public class EventsController {
 
     @GetMapping({"/addEvent","/see_events"})
     public ModelAndView createEvents(){
-        ModelAndView modelAndView = new ModelAndView("CreateEvent");
+        ModelAndView modelAndView = new ModelAndView("createEvent");
         Events ourEvents = new Events();
         modelAndView.addObject("meeting_event", ourEvents);
 
@@ -28,10 +30,22 @@ public class EventsController {
         return "redirect:/index.table";
 
     }
-    @GetMapping("/adminDashboard")
+    @GetMapping({ "/eventsOperations"})
     public ModelAndView getAllEmployees(){
-        ModelAndView mov = new ModelAndView("adminDashboard");
+        ModelAndView mov = new ModelAndView("eventsOperations");
         mov.addObject("meeting_event", eventsRepo.findAll());
         return mov;
+    }
+    @GetMapping("/update")
+    public ModelAndView showUpdate(@RequestParam Long employeeId){
+        ModelAndView mov = new ModelAndView("createEvent");
+        Events ourEvents = eventsRepo.findById(employeeId).get();
+        mov.addObject("meeting_event", ourEvents);
+        return mov;
+    }
+    @GetMapping("/deleteEmployee")
+    public String showDeleteEmployee(@RequestParam Long eventsId){
+        eventsRepo.deleteById(eventsId);
+        return "redirect:/eventsOperations";
     }
 }
