430e7f96d5db09a65a0ccea73a44abdfff7f831b
Nicholas Maundu <nickiemaundu19@gmail.com>
2021-11-01 18:24:46 +0300

    updates on admin dashboard and added calendar file

--- src/main/java/com/nicholas/officemanager/repositories/UsersRepository.java
+++ src/main/java/com/nicholas/officemanager/repositories/UsersRepository.java
@@ -3,6 +3,7 @@ package com.nicholas.officemanager.repositories;
 import com.nicholas.officemanager.entitities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,9 +11,7 @@ import java.util.Optional;
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
     @Query("SELECT u FROM Users u WHERE u.employee_email = ?1")
-    Users findByEmail(String email);
-//    Optional<Users> findByEmployee_email(String email);
-//    Optional<Employees> findByEmployee_email(String employee_email);
+    public Users getUsersByEmployee_email(@Param("employee_email")String employee_email);
 }
 
 
